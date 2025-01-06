4840ae4d6fbcbe20e57aa2ed17b403ad59aa5f6b
Rashid Magomedov <Rashid.magomedov.05.dev@gmail.com>
2025-01-06 11:26:56 +0300

    Add DetailsComponentImpl

--- details_screen/src/main/java/com/example/details_screen/presentation/DetailsComponentImpl.kt
+++ details_screen/src/main/java/com/example/details_screen/presentation/DetailsComponentImpl.kt
@@ -1,7 +1,57 @@
-package com.example.details_feature.presentation
+package com.example.details_screen.presentation
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.mvikotlin.core.instancekeeper.getStore
+import com.arkivanov.mvikotlin.extensions.coroutines.labels
+import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
+import com.example.core.extensions.componentScope
+import com.example.core.models.City
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class DetailsComponentImpl(
-    componentContext: ComponentContext
-) : DetailsComponent, ComponentContext by componentContext
+class DetailsComponentImpl @AssistedInject constructor(
+    private val detailsStoreFactory: DetailsStoreFactory,
+    @Assisted("city") private val city: City,
+    @Assisted("onBackClicked") private val onBackClicked: () -> Unit,
+    @Assisted("componentContext") componentContext: ComponentContext
+) : DetailsComponent, ComponentContext by componentContext {
+
+    private val store = instanceKeeper.getStore { detailsStoreFactory.create(city) }
+    private val scope = componentScope()
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val model: StateFlow<DetailsStore.State> = store.stateFlow
+
+    init {
+        scope.launch {
+            store.labels.collect {
+                when(it) {
+                    is DetailsStore.Label.ClickBack -> {
+                        onBackClicked()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onClickBack() {
+        store.accept(DetailsStore.Intent.ClickBack)
+    }
+
+    override fun onClickChangeFavouriteStatus() {
+        store.accept(DetailsStore.Intent.ClickChangeFavouriteStatus)
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(
+            @Assisted("city") city: City,
+            @Assisted("onBackClicked") onBackClicked: () -> Unit,
+            @Assisted("componentContext") componentContext: ComponentContext
+        ): DetailsComponentImpl
+    }
+
+}
